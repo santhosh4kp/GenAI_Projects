a8ed20081d41e9d07b3cb8879bf497da44c0908c
Santhosh Kumar K P <santhosh4kp@gmail.com>
2025-10-07 12:35:20 +0530

    day 3 11

--- Day3_saas_clerk__auth/vercel_llm/api/index.py
+++ Day3_saas_clerk__auth/vercel_llm/api/index.py
@@ -7,44 +7,10 @@ from fastapi_clerk_auth import ClerkConfig, ClerkHTTPBearer, HTTPAuthorizationCr
 
 
 client = Groq(api_key="")
-
 app = FastAPI()
 
 clerk_config = ClerkConfig(jwks_url=os.getenv("CLERK_JWKS_URL"))
 clerk_guard = ClerkHTTPBearer(clerk_config)
-
-# @app.get("/api")
-# def idea(creds: HTTPAuthorizationCredentials = Depends(clerk_guard)):
-#     user_id = creds.decoded["sub"]  # User ID from JWT - available for future use
-#     # We now know which user is making the request! 
-#     # You could use user_id to:
-#     # - Track usage per user
-#     # - Store generated ideas in a database
-#     # - Apply user-specific limits or customization
-    
-#     #print(user_id)
-#     #llm call
-#     message = "Come up with a new business idea for AI Agents"
-
-#     stream = client.chat.completions.create(
-#         model="llama-3.1-8b-instant",   # free Groq model
-#         messages=[
-#             {"role": "user", "content": message}
-#         ], stream=True
-#     )
-
-#     # def event_stream():
-#     #     for chunk in stream:
-#     #         text = chunk.choices[0].delta.content
-#     #         if text:
-#     #             lines = text.split("\n")
-#     #             for line in lines[:-1]:
-#     #                 yield f"data: {line}\n\n"
-#     #                 yield "data:  \n"
-#     #             yield f"data: {lines[-1]}\n\n"
-#     #return StreamingResponse(event_stream(), media_type="text/event-stream")
-#     return stream
-
 
 @app.get("/api")
 def idea(creds: HTTPAuthorizationCredentials = Depends(clerk_guard)):
@@ -57,7 +23,6 @@ def idea(creds: HTTPAuthorizationCredentials = Depends(clerk_guard)):
         messages=[{"role": "user", "content": message}],
         stream=True
     )
-
     def event_stream():
         for chunk in stream:
             text = chunk.choices[0].delta.content
@@ -67,6 +32,4 @@ def idea(creds: HTTPAuthorizationCredentials = Depends(clerk_guard)):
                     yield f"data: {line}\n\n"
                     yield "data:  \n"
                 yield f"data: {lines[-1]}\n\n"
-
-
     return StreamingResponse(event_stream(), media_type="text/event-stream")
